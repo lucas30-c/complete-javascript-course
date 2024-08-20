9e6472868f9a57129ef17215c3483128adb3dc09
lucas30-c <96409387+lucas30-c@users.noreply.github.com>
2024-08-20 10:43:09 +1000

    123- string2 trim, replace, replaceAll

---
+++ 09-Data-Structures-Operators/starter/123- string 2.js
@@ -0,0 +1,36 @@
+const airline = 'TAP Air Portugal';
+const plane = 'A320';
+
+console.log(airline.toUpperCase());
+console.log(airline.toLowerCase());
+
+// to fix capitalization in name
+const passenger = 'jOnAS'; // Jonas
+const passengerLower = passenger.toLowerCase();
+const passengerCorrect =
+  passengerLower[0].toUpperCase() + passengerLower.slice(1);
+console.log(passengerCorrect);
+
+// Comparing emails
+const email = 'hello@jonas.io';
+const loginEmail = ' Hello@Jonas.Io \n';
+
+const lowerEmail = loginEmail.toLowerCase();
+const trimmedEmail = lowerEmail.trim();
+console.log(trimmedEmail);
+
+const normalizedEmail = loginEmail.toLowerCase().trim();
+console.log(normalizedEmail);
+console.log(email === normalizedEmail);
+
+// replacing
+const priceGB = '288,97£';
+const priceUS = priceGB.replace('£', '$').replace(',', '.');
+console.log(priceUS);
+
+const announcement =
+  'All passengers come to boarding door 23. Boarding door 23!';
+console.log(announcement.replaceAll('door', 'gate')); // replaceAll() replaces all occurrences of a specified value.
+// It is working since node version 15.0.0
+
+console.log(announcement.replace(/door/g, 'gate')); // Regular expression
