e454bb6e56f57302ce957b09c3f042d9c86335a1
lucas30-c <96409387+lucas30-c@users.noreply.github.com>
2024-08-29 17:34:03 +1000

    p212-Prototypal Inheritance
    console.log(jonas.hasOwnProperty('species')); // false
    
    console.log(jonas.__proto__.__proto__);
    // The prototype property of the Object
    console.log(jonas.__proto__.__proto__.__proto__);
    // null
    
    console.dir(Person.prototype.constructor); // ƒ (firstName, birthYear) {...}
    
    // Prototype of arrays
    
    const arr = [3, 6, 6, 5, 6, 9, 9]; // new Array === []
    console.log(arr.__proto__); // Array(0)
    console.log(arr.__proto__ === Array.prototype); // true
    console.log(arr.__proto__.__proto__); // [Object: null prototype] {}
    
    Array.prototype.unique = function () {
      return [...new Set(this)];
    };
    
    console.log(arr.unique()); // [3, 6, 5, 9]
    
    const h1 = document.querySelector('h1');
    console.dir(h1);
    console.dir(h1.__proto__); // HTMLHeadingElement => HTMLElement => Element => Node => EventTarget => Object => null

--- 14-OOP/starter/210-prototypes.js
+++ 14-OOP/starter/210-prototypes.js
@@ -39,3 +39,29 @@ console.log(jonas.species, matilda.species); // Homer Sapiens Homer Sapiens
 
 console.log(jonas.hasOwnProperty('firstName')); // true
 console.log(jonas.hasOwnProperty('species')); // false
+
+console.log(jonas.__proto__.__proto__);
+// The prototype property of the Object
+console.log(jonas.__proto__.__proto__.__proto__);
+// null
+
+console.dir(Person.prototype.constructor); // ƒ (firstName, birthYear) {...}
+
+// Prototype of arrays
+
+const arr = [3, 6, 6, 5, 6, 9, 9]; // new Array === []
+console.log(arr.__proto__); // Array(0)
+console.log(arr.__proto__ === Array.prototype); // true
+console.log(arr.__proto__.__proto__); // [Object: null prototype] {}
+
+Array.prototype.unique = function () {
+  return [...new Set(this)];
+};
+
+console.log(arr.unique()); // [3, 6, 5, 9]
+
+const h1 = document.querySelector('h1');
+console.dir(h1);
+console.dir(h1.__proto__); // HTMLHeadingElement => HTMLElement => Element => Node => EventTarget => Object => null
+
+console.dir(x => x + 1);
