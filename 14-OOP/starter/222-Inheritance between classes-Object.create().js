731551c5cf4f761cc6ae9e17b61645015fc08f63
lucas30-c <96409387+lucas30-c@users.noreply.github.com>
2024-08-31 16:10:12 +1000

    p222-Inheritance between classes: Object.create()

---
+++ 14-OOP/starter/222-Inheritance between classes-Object.create().js
@@ -0,0 +1,31 @@
+const PersonProto = {
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  },
+
+  init(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  },
+};
+
+// PersonProto is the prototype of steven
+// This will return a brand new object that is linked to PersonProto
+const steven = Object.create(PersonProto);
+
+// PersonProto is the prototype of StudentProto
+// StudentProto is the prototype of jay
+const StudentProto = Object.create(PersonProto);
+StudentProto.init = function (firstName, birthYear, course) {
+  PersonProto.init.call(this, firstName, birthYear);
+  this.course = course;
+};
+
+StudentProto.introduce = function () {
+  console.log(`My name is ${this.firstName} and I study ${this.course}`);
+};
+
+const jay = Object.create(StudentProto);
+jay.init('Jay', 2010, 'Computer Science');
+jay.introduce();
+jay.calcAge();
