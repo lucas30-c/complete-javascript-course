db4d4c5474b0b9720d2ad7064d64a0987f5476ce
lucas30-c <96409387+lucas30-c@users.noreply.github.com>
2024-09-02 23:01:04 +1000

    p79~80 modal window and working with classes
    const openModal = function () {
      modal.classList.remove('hidden');
      overlay.classList.remove('hidden');
    };
    
    const closeModal = function () {
      modal.classList.add('hidden');
      overlay.classList.add('hidden');
    };
    
    // Will get a NodeList
    console.log(btnsOpenModal);
    
    for (let i = 0; i < btnsOpenModal.length; i++) {
      console.log(btnsOpenModal[i].textContent);
      btnsOpenModal[i].addEventListener('click', openModal);
    }

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,31 @@
 'use strict';
+
+// console.log(document.querySelector('.message').textContent);
+
+// document.querySelector('.message').textContent = '🎉 Correct Number!';
+
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.guess').value = 23;
+
+const secretNmuber = Math.trunc(Math.random() * 20) + 1; // only get the integer part
+let score = 20;
+document.querySelector('.number').textContent = secretNmuber;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔️ No number!';
+  } else if (guess === secretNmuber) {
+    document.querySelector('.message').textContent = '🎉 Correct Number!';
+  } else if (guess > secretNmuber) {
+    document.querySelector('.message').textContent = '📈 Too high!';
+    score--;
+    document.querySelector('.score').textContent = score;
+  } else if (guess < secretNmuber) {
+    document.querySelector('.message').textContent = '📉 Too low!';
+    score--;
+    document.querySelector('.score').textContent = score;
+  }
+});
--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,30 @@
 'use strict';
+
+// Selecting elements
+const modal = document.querySelector('.modal');
+
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+// Will get a NodeList
+console.log(btnsOpenModal);
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  console.log(btnsOpenModal[i].textContent);
+  btnsOpenModal[i].addEventListener('click', openModal);
+}
+
+btnCloseModal.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
