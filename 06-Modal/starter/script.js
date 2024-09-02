93311857c9127146a53848a20f148308cd74ebe0
lucas30-c <96409387+lucas30-c@users.noreply.github.com>
2024-09-02 23:18:03 +1000

    p82 Handling the ESC keypress event
    // Keyboard events are global events, we listen on the document
    
    document.addEventListener('keydown', function (e) {
      console.log(e);
    // 严谨性
      if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
        closeModal();
      }
    });

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -28,3 +28,12 @@ for (let i = 0; i < btnsOpenModal.length; i++) {
 btnCloseModal.addEventListener('click', closeModal);
 
 overlay.addEventListener('click', closeModal);
+
+// Keyboard events are global events, we listen on the document
+
+document.addEventListener('keydown', function (e) {
+  console.log(e);
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
