d0a0d6de22b5df4f30b3f0fe031e83d6664b9ee0
lucas30-c <96409387+lucas30-c@users.noreply.github.com>
2024-09-03 21:49:48 +1000

    192(Important)-Event Propagation
    Bubble up:
    It is as if the event also happens at the parent element, and then at the grandparent element, and so on
    For all 3 event listeners, the target element is the same, but the currentTarget is different
    The parameter e here is the same for all 3 event listeners
    So we can handle that event in all the parent elements
    The current target: the element that the event listener is attached to(the this keyword)
    
    The bubbling phase is very useful because it allows us to implement event delegation
    
    // rgb(255, 255, 255)
    const randomInt = (min, max) =>
      Math.floor(Math.random() * (max - min + 1) + min);
    
    const randomColor = () =>
      `rgb(${randomInt(0, 255)}, ${randomInt(0, 255)}, ${randomInt(0, 255)})`;
    
    // Bubble up:
    // It is as if the event also happens at the parent element, and then at the grandparent element, and so on
    // For all 3 event listeners, the target element is the same, but the currentTarget is different
    // The parameter e here is the same for all 3 event listeners
    // So we can handle that event in all the parent elements
    // The current target: the element that the event listener is attached to(the this keyword)
    
    // The bubbling phase is very useful because it allows us to implement event delegation
    
    document.querySelector('.nav__link').addEventListener('click', function (e) {
      this.style.backgroundColor = randomColor();
      // e.target: where the event originated(where the event first happened)
      console.log('LINK', e.target, e.currentTarget);
    
      // Stop propagation
      // If we add e.stopPropagation(), the event will not bubble up anymore(It will never arrive at the parent element) So the click event will only happen on the link
      // e.stopPropagation();
    });
    
    document.querySelector('.nav__links').addEventListener('click', function (e) {
      this.style.backgroundColor = randomColor();
      console.log('CONTAINER', e.target, e.currentTarget);
      console.log(e.currentTarget === this); // true
    });
    
    // The capturing phase
    // If we add 'true' as the third argument, the event listener will be executed during the capturing phase. So 'NAV' will be logged first, then 'LINK', and then 'CONTAINER'
    // This element will actually listen for event as it travels down from the DOM
    document.querySelector('.nav').addEventListener(
      'click',
      function (e) {
        this.style.backgroundColor = randomColor();
        console.log('NAV', e.target, e.currentTarget);
      },
      true
    );

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -77,3 +77,48 @@ setTimeout(() => {
 // h1.onmouseenter = function(e) {
 //   alert('onmouseenter: Great! You are reading the heading :D');
 // }
+
+// rgb(255, 255, 255)
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1) + min);
+
+const randomColor = () =>
+  `rgb(${randomInt(0, 255)}, ${randomInt(0, 255)}, ${randomInt(0, 255)})`;
+
+// Bubble up:
+// It is as if the event also happens at the parent element, and then at the grandparent element, and so on
+// For all 3 event listeners, the target element is the same, but the currentTarget is different
+// The parameter e here is the same for all 3 event listeners
+// So we can handle that event in all the parent elements
+// The current target: the element that the event listener is attached to(the this keyword)
+
+// The bubbling phase is very useful because it allows us to implement event delegation
+
+document.querySelector('.nav__link').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  // e.target: where the event originated(where the event first happened)
+  console.log('LINK', e.target, e.currentTarget);
+
+  // Stop propagation
+  // If we add e.stopPropagation(), the event will not bubble up anymore(It will never arrive at the parent element) So the click event will only happen on the link
+  // e.stopPropagation();
+});
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log('CONTAINER', e.target, e.currentTarget);
+  console.log(e.currentTarget === this); // true
+});
+
+
+// The capturing phase
+// If we add 'true' as the third argument, the event listener will be executed during the capturing phase. So 'NAV' will be logged first, then 'LINK', and then 'CONTAINER'
+// This element will actually listen for event as it travels down from the DOM
+document.querySelector('.nav').addEventListener(
+  'click',
+  function (e) {
+    this.style.backgroundColor = randomColor();
+    console.log('NAV', e.target, e.currentTarget);
+  },
+  true
+);
